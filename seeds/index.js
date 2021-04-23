092b9ee561391f79490cfef372159ec32759ab68
Aaron <aronchernick@gmail.com>
2021-04-23 15:12:09 -0400

    third commit

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-    const camp = new Campground ({
+    const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground ({
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        title: `${sample(descriptors)} ${sample(places)}`
+        title: `${sample(descriptors)} ${sample(places)}`,
+        image: "https://unsplash.com/collections/483251/in-the-woods",
+        description: 'Lorem, ipsum dolor sit amet consectetur adiLorempisicing elit. Consequuntur nam reprehenderit explicabo ratione aspernatur aliquid repellat laboriosam, culpa architecto id quaerat quis ad, perferendis, exercitationem repellendus at voluptates. Earum, provident.',
+    price
     })
     await camp.save();
     }
